0bb06c95583ad627a4cfa9f8f3bb3ddd90f0ff61
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-24 03:34:58 +0300

    The landing page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Free 10GB for Airtel UG users</title>
+</head>
+
+<body>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4700591257043183" crossorigin="anonymous"></script>
+</body>
+
+</html>
