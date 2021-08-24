7d07a4b38971043e01ced3637548601958e14b56
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-24 04:20:43 +0300

    Added ads

--- index.php
+++ index.php
@@ -6,11 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Free 10GB for Airtel UG users</title>
+    <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872580992062840"
+     crossorigin="anonymous"></script> -->
     <script data-ad-client="ca-pub-9872580992062840" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body>
-    
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9872580992062840" data-ad-slot="6915706332" data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <section id="newsfeed-test">
+        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-gl+12+75-1p-7y" data-ad-client="ca-pub-9872580992062840" data-ad-slot="2533608199"></ins>
+        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-gl+12+75-1p-7y" data-ad-client="ca-pub-9872580992062840" data-ad-slot="2533608199"></ins>
+        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-gl+12+75-1p-7y" data-ad-client="ca-pub-9872580992062840" data-ad-slot="2533608199"></ins>
+        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-gl+12+75-1p-7y" data-ad-client="ca-pub-9872580992062840" data-ad-slot="2533608199"></ins>
+        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-gl+12+75-1p-7y" data-ad-client="ca-pub-9872580992062840" data-ad-slot="2533608199"></ins>
+    </section>
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9872580992062840" data-ad-slot="6915706332" data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <!-- horizontal ads -->
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9872580992062840" data-ad-slot="4038261550" data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </body>
 
 </html>
