88be606791f211268d0cf17ec2e839ca9daffbd2
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-24 04:02:55 +0300

    Readme added

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Free 10GB for Airtel UG users</title>
+    <script data-ad-client="ca-pub-9872580992062840" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4700591257043183" crossorigin="anonymous"></script>
+    
 </body>
 
 </html>
