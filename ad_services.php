e51e8af31d007d523c0e6d27ace5de452dad9323
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-28 22:19:13 +0300

    refactor ads

--- ad_services.php
+++ ad_services.php
@@ -1,5 +1,9 @@
+<!--Ad Services Or Ad Networks-->
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872580992062840"
         crossorigin="anonymous"></script>
 <script type="text/javascript" data-adel="atag" src="//acscdn.com/script/atg.js" czid="bqdrwbiz"></script>
+<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4482088,document.body||document.documentElement)</script>
+<script type="text/javascript"> var infolinks_pid = 3342412; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
+<!-- Verifications -->
 <meta name="a.validate.01" content="68f4d9cc7ea4b9099e227692d9d6374e95d5" />
 <meta name="propeller" content="0fa91932dfef1f8d1e1516fbbe616fa1">
